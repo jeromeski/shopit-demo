f3853ccc60094ec74b3aee841d73373c4a8d8f30
jeromeski <jeromegee@gmail.com>
2021-12-16 13:25:12 +0800

    Filter Products

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -13,8 +13,24 @@ class APIFeatures {
 					}
 			  }
 			: {};
-		console.log(keyword);
+
 		this.query = this.query.find({ ...keyword });
+		return this;
+	}
+
+	filter() {
+		const queryCopy = { ...this.queryStr };
+		console.log(queryCopy);
+
+		// Removing fields from the query string
+		const removeFields = ["keyword", "limit", "page"];
+		removeFields.forEach((el) => delete queryCopy[el]);
+
+		// Advance filter for price, ratings etc
+		let queryStr = JSON.stringify(queryCopy);
+		queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+		this.query = this.query.find(JSON.parse(queryStr));
 		return this;
 	}
 }
