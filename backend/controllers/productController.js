ca769e9119a5e555374eadbefa143bf36e0cbcfc
jeromeski <jeromegee@gmail.com>
2021-12-16 08:16:54 +0800

    Search products by keword

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/product");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsynErrors");
+const APIFeatures = require("../utils/apiFeatures");
 
 // Create new product ==> /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
@@ -13,11 +14,11 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get all products ==> /api/v1/products
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-	const products = await Product.find();
+	const apiFeatures = new APIFeatures(Product.find(), req.query).search();
+	const products = await apiFeatures.query;
 
 	res.status(200).json({
 		success: true,
-		message: "This route will show all products in database",
 		count: products.length,
 		products
 	});
