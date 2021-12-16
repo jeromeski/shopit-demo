48107bdd808a47b2d1fd76fe40ffa46d2f2dc6e6
jeromeski <jeromegee@gmail.com>
2021-12-16 16:04:33 +0800

    Adding Pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -14,7 +14,12 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get all products ==> /api/v1/products
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-	const apiFeatures = new APIFeatures(Product.find(), req.query).search().filter();
+  const resPerPage = 4;
+	const productCount = await Product.countDocuments();
+	const apiFeatures = new APIFeatures(Product.find(), req.query)
+		.search()
+		.filter()
+		.pagination(resPerPage);
 	const products = await apiFeatures.query;
 
 	res.status(200).json({
