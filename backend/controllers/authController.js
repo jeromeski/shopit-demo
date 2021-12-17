538c5260d84fff5afa7a756b37913c27fef56c7d
jeromeski <jeromegee@gmail.com>
2021-12-18 07:33:53 +0800

    Authorize user roles and permissions

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -57,3 +57,4 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
   res.cookie("token", null, {expires: new Date(Date.now()), httpOnly: true})
   res.status(200).json({success: true, message: "Logged out"})
 })
+
