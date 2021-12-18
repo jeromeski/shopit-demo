7f0008204ee94b1262b901e7e8b594e5cfbd78b6
jeromeski <jeromegee@gmail.com>
2021-12-18 16:39:38 +0800

    Show user profile

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,14 +6,18 @@ const {
 	loginUser,
 	logout,
 	forgotPassword,
-	resetPassword
+	resetPassword,
+	getUserProfile
 } = require("../controllers/authController");
+
+const { isAuthenticatedUser } = require("../middleware/auth");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logout);
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
 
 
 module.exports = router;
