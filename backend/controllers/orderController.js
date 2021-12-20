3c7e5c8d5030fb8eaa70796f68a9e83f6ea900ac
jeromeski <jeromegee@gmail.com>
2021-12-20 08:31:07 +0800

    Get single order & Logged in user

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -26,3 +26,22 @@ exports.newOrder = catchAsynErrors(async (req, res, next) => {
 		order
 	});
 });
+
+// Get single order => /api/v1/order/:id
+exports.getSingleOrder = catchAsynErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate("user", "name email");
+
+  if(!order) {
+    return next(new ErrorHandler("No order found with this ID", 404))
+  }
+
+  res.status(200).json({success: true, order})
+});
+
+// Get loggedin user orders => /api/v1/orders/me
+exports.myOrders = catchAsynErrors(async (req, res, next) => {
+  const orders = await Order.find({user: req.user.id})
+
+  res.status(200).json({success: true, orders})
+})
+
