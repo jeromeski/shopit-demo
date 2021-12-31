0657b7afa6bed6c8d8bf4ff4df8af8a367fc151e
jeromeski <jeromegee@gmail.com>
2021-12-31 12:17:30 +0800

    Process payment with strife

--- frontend/src/components/cart.js/Payment.js
+++ frontend/src/components/cart.js/Payment.js
@@ -6,6 +6,7 @@ import CheckoutSteps from "./CheckoutSteps";
 import { useAlert } from "react-alert";
 import { useDispatch, useSelector } from "react-redux";
 
+
 import {
 	useStripe,
 	useElements,
@@ -38,8 +39,78 @@ const Payment = ({ history }) => {
 
 	useEffect(() => {}, []);
 
-	const submitHandler = (e) => {
-		//
+	const order = {
+		orderItems: cartItems,
+		shippingInfo
+	};
+
+	const orderInfo = JSON.parse(sessionStorage.getItem("orderInfo"));
+	if (orderInfo) {
+		order.itemsPrice = orderInfo.itemsPrice;
+		order.shippingPrice = orderInfo.shippingPrice;
+		order.taxPrice = orderInfo.taxPrice;
+		order.totalPrice = orderInfo.totalPrice;
+	}
+
+	const paymentData = {
+		amount: Math.round(orderInfo.totalPrice * 100)
+	};
+
+	const submitHandler = async (e) => {
+		e.preventDefault();
+
+		document.querySelector("#pay_btn").disabled = true;
+
+		let res;
+		try {
+			const config = {
+				headers: {
+					"Content-Type": "application/json"
+				}
+			};
+
+			res = await axios.post("/api/v1/payment/process", paymentData, config);
+
+			const clientSecret = res.data.client_secret;
+
+			console.log(clientSecret);
+
+			if (!stripe || !elements) {
+				return;
+			}
+
+			const result = await stripe.confirmCardPayment(clientSecret, {
+				payment_method: {
+					card: elements.getElement(CardNumberElement),
+					billing_details: {
+						name: user.name,
+						email: user.email
+					}
+				}
+			});
+
+			if (result.error) {
+				alert.error(result.error.message);
+				document.querySelector("#pay_btn").disabled = false;
+			} else {
+				// The payment is processed or not
+				if (result.paymentIntent.status === "succeeded") {
+					order.paymentInfo = {
+						id: result.paymentIntent.id,
+						status: result.paymentIntent.status
+					};
+
+					// Todo new order
+
+					history.push("/success");
+				} else {
+					alert.error("There is some issue while payment processing");
+				}
+			}
+		} catch (error) {
+			document.querySelector("#pay_btn").disabled = false;
+			alert.error(error.response.data.message);
+		}
 	};
 
 	return (
@@ -83,7 +154,7 @@ const Payment = ({ history }) => {
 						</div>
 
 						<button id="pay_btn" type="submit" className="btn btn-block py-3">
-							Pay $1000
+							Pay {` - ${orderInfo && orderInfo.totalPrice}`}
 						</button>
 					</form>
 				</div>
