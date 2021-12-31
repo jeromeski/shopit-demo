b7d66b8891cd3692b207efe3a94d205726802d1a
jeromeski <jeromegee@gmail.com>
2021-12-31 10:00:18 +0800

    Adding stripe on Backend

--- backend/app.js
+++ backend/app.js
@@ -20,10 +20,12 @@ app.use(morgan("dev"));
 const products = require("./routes/product");
 const auth = require("./routes/auth");
 const order = require("./routes/order");
+const payment = require("./routes/payment");
 
 app.use("/api/v1", products);
 app.use("/api/v1", auth);
 app.use("/api/v1", order);
+app.use("/api/v1", payment);
 
 // Middleware to handle errors
 app.use(errorMiddleware);
---
+++ backend/controllers/paymentController.js
@@ -0,0 +1,18 @@
+const catchAsynErrors = require("../middleware/catchAsynErrors");
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+// Process stripe payments   =>   /api/v1/payment/process
+exports.processPayment = catchAsynErrors(async (req, res, next) => {
+	const paymentIntent = await stripe.paymentIntents.create({
+		amount: req.body.amount,
+		currency: "usd",
+
+		metadata: { integration_check: "accept_a_payment" }
+	});
+
+	res.status(200).json({
+		success: true,
+		client_secret: paymentIntent.client_secret
+	});
+});
